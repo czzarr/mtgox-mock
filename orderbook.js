7308937112d4bade9a5dbc78ddbd99e7233802c6
Stanislas Marion <stanislas.marion@gmail.com>
2014-01-22 16:58:17 +0100

    trade works

--- orderbook.js
+++ orderbook.js
@@ -1,84 +1,90 @@
-var Transform = require('stream').Transform;
-var util = require('util');
+var util = require('util')
+var stream = require('stream')
 
-util.inherits(Orderbook, Transform);
+module.exports = function () {
+  util.inherits(Orderbook, stream.Transform);
 
-function Orderbook () {
-  Transform.call(this);
-  this._writableState.objectMode = true;
-  this.ask;
-  this.bid;
-}
+  function Orderbook () {
+    if (!(this instanceof Orderbook)) return new Orderbook();
+    stream.Transform.call(this);
+    this._writableState.objectMode = true;
+    this.ask;
+    this.bid;
+  }
 
-Orderbook.prototype._transform = function (chunk, encoding, done) {
-  switch (chunk.type) {
-    case 'order':
-      var order = chunk.order;
-      if (order.type === 'add') {
-        this.add(chunk.order);
-        //this.push('add');
-        // TODO
-        //this.push(user_order pending post pending executing open);
+  Orderbook.prototype._transform = function (chunk, encoding, done) {
+    switch (chunk.type) {
+      case 'order':
+        var order = chunk.order;
+        if (order.type === 'add') {
+          this.add(chunk.order);
+          //this.push('add');
+          // TODO
+          //this.push(user_order pending post pending executing open);
+        }
+        if (order.type === 'cancel') {
+          this.cancel(chunk.orderId);
+          //this.push('cancel');
+          // TODO
+          //this.push(user_order reason requested);
+        }
+        break;
+      case 'trade':
+          //this.push('trade');
+          // TODO
+          this.push(this.trade(chunk.trade));
+        break;
+    }
+    done();
+  };
+
+  Orderbook.prototype.add = function (order) {
+    if (order.type === 'ask') {
+      this.ask = order;
+    } else if (order.type === 'bid') {
+      this.bid = order;
+    }
+    console.log(this.ask, this.bid);
+  }
+
+  Orderbook.prototype.cancel = function (orderId) {
+    if (this.ask.oid === orderId) {
+      this.ask = null;
+    } else if (this.bid.oid === orderId) {
+      this.bid = null;
+    }
+  }
+
+  Orderbook.prototype.trade = function (trade) {
+    if (trade.trade_type === 'bid') {
+      if (this.ask && trade.price_int === this.ask.price_int) {
+        if (this.ask.amount_int <= trade.amount_int) {
+          this.ask = null;
+          // TODO
+          return { result: 'order_completed', trade: trade, type: 'trade', orderId: this.ask.oid };
+        } else {
+          this.ask.amount_int -= trade.amount_int;
+          // TODO
+          return { result: 'order_partially_filled', trade: trade, type: 'trade', orderId: this.ask.oid };
+        }
       }
-      if (order.type === 'cancel') {
-        this.cancel(chunk.orderId);
-        //this.push('cancel');
-        // TODO
-        //this.push(user_order reason requested);
-      }
-      break;
-    case 'trade':
-        //this.push('trade');
-        // TODO
-        this.push(this.trade(chunk.trade));
-      break;
-  }
-  done();
-};
-
-Orderbook.prototype.add = function (order) {
-  if (order.type === 'ask') {
-    this.ask = order;
-  } else if (order.type === 'bid') {
-    this.bid = order;
-  }
-}
-
-Orderbook.prototype.cancel = function (orderId) {
-  if (this.ask.oid === orderId) {
-    this.ask = null;
-  } else if (this.bid.oid === orderId) {
-    this.bid = null;
-  }
-}
-
-Orderbook.prototype.trade = function (trade) {
-  if (trade.trade_type === 'bid') {
-    if (this.ask && trade.price_int === this.ask.price_int) {
-      if (this.ask.volume_int <= trade.volume_int) {
-        this.ask = null;
-        // TODO
-        return { result: 'order_completed', trade: trade, type: 'trade', orderId: this.ask.oid };
-      } else {
-        this.ask.volume_int -= trade.volume_int;
-        // TODO
-        return { result: 'order_partially_filled', trade: trade, type: 'trade', orderId: this.ask.oid };
+    }
+    if (trade.trade_type === 'ask') {
+      if (this.bid && trade.price_int === this.bid.price_int) {
+        if (parseInt(this.bid.amount_int, 10) <= parseInt(trade.amount_int, 10)) {
+          this.bid = null;
+          // TODO
+          this.emit('order_completed', trade);
+        } else {
+          this.bid.amount_int -= trade.amount_int;
+          // TODO
+          this.emit('order_partially_filled', trade);
+        }
       }
     }
   }
-  if (trade.trade_type === 'ask') {
-    if (this.bid && trade.price_int === this.bid.price_int) {
-      if (this.bid.volume_int <= trade.volume_int) {
-        this.bid = null;
-        // TODO
-        this.emit('order_completed', trade);
-      } else {
-        this.bid.volume_int -= trade.volume_int;
-        // TODO
-        this.emit('order_partially_filled', trade);
-      }
-    }
-  }
+
+  return new Orderbook()
 }
 
-module.exports = Orderbook;
+
--- socket.js
+++ socket.js
@@ -2,12 +2,13 @@ var MongoClient = require('mongodb').MongoClient;
 var io = require('socket.io').listen(3001);
 var IntervalStream = require('interval-stream');
 var through = require('through');
+var express = require('express');
+var replify = require('replify')
 var config = require('./config');
 var messages = require('./messages');
-var Orderbook = require('./orderbook');
+var orderbook = require('./orderbook')();
 var socketStream = require('./socket-stream');
 var is = new IntervalStream(1000, true);
-var orderbook = new Orderbook();
 
 io.on('connection', function (socket) {
   var emitter = socketStream(socket);
@@ -30,3 +31,24 @@ io.on('connection', function (socket) {
     }
   });
 });
+
+var app = express();
+app.use(express.json());
+app.use(app.router);
+var server = app.listen(3000);
+
+app.get('/test', function (req, res) {
+  res.send(200, 'test worked');
+});
+
+app.post('/money/order/add', function (req, res) {
+  var order = { amount_int: req.body.amount_int, price_int: req.body.price_int, type: req.body.type };
+  orderbook.add(order);
+  res.send(200, 'order added');
+});
+
+app.post('/money/order/cancel', function (req, res) {
+  orderbook.cancel(req.body.oid);
+  res.send(200, 'order cancelled');
+});
+replify('socket', orderbook)
